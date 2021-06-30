3954f8940cb580c3896347492591acd6174cf65b
devDeveloperMohsin <mohsin.ali8541@gmail.com>
2021-06-30 10:36:05 +0500

    Todo App Design

--- src/components/todo/Todo.vue
+++ src/components/todo/Todo.vue
@@ -44,18 +44,25 @@ export default {
     border: 1px solid black;
     padding: 15px;
     font-family: Arial, Helvetica, sans-serif;
+    background-color: #8601ff;
   }
   h1{
     text-align: center;
-    color: red;
+    color: #333333;
+    text-transform: uppercase;
   }
   input{
     width: 100%;
     box-sizing: border-box;
     padding: 10px;
-    border: 1px solid #f1f1f1;
+    border: none;
     outline: none;
-    box-shadow: 0px 3px #dedede;
+    box-shadow: 0px 3px #fbff00;
+    background-color: #333333;
+    color: #fff;
+  }
+  input::placeholder{
+    color: #fff;
   }
   input.active{
     box-shadow: 0px 3px #00ff43;
@@ -68,22 +75,24 @@ export default {
   }
   ul li{
     padding: 5px 15px;
-    border: 1px solid #f1f1f1;
+    border: 1px solid #333333;
     display:flex;
     font-size: 12px;
     color: #333;
+    background-color: #fff;
   }
   ul li + li{
     border-top: none;
   }
   ul li button{
     outline: none;
-    border: 1px solid #f1f1f1;
+    border: 1px solid #bdbdbd;
     background-color: transparent;
     margin-left: auto;
     height: 20px;
     width: 20px;
     font-size: 10px;
+    margin-right: -8px;
   }
   ul li button:hover{
     border-color: red;
